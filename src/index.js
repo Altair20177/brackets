a01d8e4221c3f37482ca8d48048dd5e4b13161f9
Maksim <78382480+Altair20177@users.noreply.github.com>
2021-03-13 18:43:22 +0300

    refactor: change fnction

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 module.exports = function check(str, bracketsConfig) {
   str = str.split('');
   let brackets = [];
-  let tmp1 = 0;
-  let tmp2 = 0;
   for(let i = 0; i < bracketsConfig.length; i++)
   {
     brackets[i] = 0;
@@ -15,25 +13,12 @@ module.exports = function check(str, bracketsConfig) {
       {
         brackets[j]++;
       }
-      if(str[i] == bracketsConfig[j][1])
+      else if(str[i] == bracketsConfig[j][1])
       {
         brackets[j]--;
         if (brackets[j] == -1) return false;
       }
     }
-  }
-  for(let i = 0; i < str.length - 1; i++){
-    tmp1 = tmp2 = -1;
-    for(let j = 0; j < bracketsConfig.length; j++){
-      if(str[i] == bracketsConfig[j][0])
-      tmp1 = j;
-    }
-    for(let j = 0; j < bracketsConfig.length; j++){
-      if(str[i+1] == bracketsConfig[j][1])
-      tmp2 = j;
-    }
-    if (tmp1 == -1 || tmp2 == -1) continue;
-    if (tmp1 != tmp2) return false;
   }
   return true;
 }
